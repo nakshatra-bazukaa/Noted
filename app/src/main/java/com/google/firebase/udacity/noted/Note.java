cb49d64d1d5eaa01e5b7fd85c863728a5c8e5eb1
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-01-11 22:08:08 +0530

    room entity for note added

---
+++ app/src/main/java/com/google/firebase/udacity/noted/Note.java
@@ -0,0 +1,39 @@
+package com.google.firebase.udacity.noted;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+
+    private String note;
+
+    private int timeStamp;
+
+    public Note(String title, String note, int timeStamp) {
+        this.title = title;
+        this.note = note;
+        this.timeStamp = timeStamp;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getNote() { return note; }
+
+    public void setNote(String note) { this.note = note; }
+
+    public int getTimeStamp() { return timeStamp; }
+
+    public void setTimeStamp(int timeStamp) { this.timeStamp = timeStamp; }
+}
