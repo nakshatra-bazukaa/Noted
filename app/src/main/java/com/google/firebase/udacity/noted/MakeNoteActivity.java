0f0b7fb83dcec958ccb85e755c7adbb91bd38d17
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-01-12 21:53:51 +0530

    MakeNoteActivity added

---
+++ app/src/main/java/com/google/firebase/udacity/noted/MakeNoteActivity.java
@@ -0,0 +1,14 @@
+package com.google.firebase.udacity.noted;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_note);
+    }
+}
