c1328e884cbafe5f55e603b78f280f50dd1a8c4e
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-01-12 15:29:17 +0530

    NoteViewModel added

---
+++ app/src/main/java/com/google/firebase/udacity/noted/NoteViewModel.java
@@ -0,0 +1,37 @@
+package com.google.firebase.udacity.noted;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository noteRepository;
+    private LiveData<List<Note>> notes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+
+        noteRepository = new NoteRepository(application);
+        notes = noteRepository.getNotes();
+    }
+
+    public void insert(Note note){
+        noteRepository.insert(note);
+    }
+
+    public  void update(Note note){
+        noteRepository.update(note);
+    }
+
+    public void delete(Note note){
+        noteRepository.update(note);
+    }
+
+    public LiveData<List<Note>> getNotes(){
+        return notes;
+    }
+}
