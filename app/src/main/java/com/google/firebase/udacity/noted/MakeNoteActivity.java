5c1116683384b6887189b1c403ed12ab40c5d821
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-01-13 22:48:57 +0530

    save button added in MakeNoteActivity

--- app/src/main/java/com/google/firebase/udacity/noted/MakeNoteActivity.java
+++ app/src/main/java/com/google/firebase/udacity/noted/MakeNoteActivity.java
@@ -1,10 +1,14 @@
 package com.google.firebase.udacity.noted;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -66,4 +70,21 @@ public class MakeNoteActivity extends AppCompatActivity {
         saveNote();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.act_make_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.save_note: saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
