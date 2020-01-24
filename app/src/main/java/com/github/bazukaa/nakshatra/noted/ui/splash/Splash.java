f45d5aa14aca0be8053450c0e8fd2d27d8628062
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-01-24 15:43:38 +0530

    FEATURE ADDED: Splash added

---
+++ app/src/main/java/com/github/bazukaa/nakshatra/noted/ui/splash/Splash.java
@@ -0,0 +1,29 @@
+package com.github.bazukaa.nakshatra.noted.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.github.bazukaa.nakshatra.noted.R;
+import com.github.bazukaa.nakshatra.noted.ui.displaynotes.MainActivity;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(Splash.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
