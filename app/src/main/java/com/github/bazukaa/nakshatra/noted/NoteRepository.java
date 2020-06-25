dd72f6a9814d415bbb8e0a696cbfa3837f42fb0c
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-06-26 00:27:31 +0530

    Utility for TrashNoteAsyncTask added

--- app/src/main/java/com/github/bazukaa/nakshatra/noted/NoteRepository.java
+++ app/src/main/java/com/github/bazukaa/nakshatra/noted/NoteRepository.java
@@ -1,10 +1,7 @@
 package com.github.bazukaa.nakshatra.noted;
 
 import android.app.Application;
-import android.os.AsyncTask;
-
 import androidx.lifecycle.LiveData;
-
 import com.github.bazukaa.nakshatra.noted.db.NoteDatabase;
 import com.github.bazukaa.nakshatra.noted.db.TrashNoteDatabase;
 import com.github.bazukaa.nakshatra.noted.db.dao.NoteDao;
@@ -12,7 +9,7 @@ import com.github.bazukaa.nakshatra.noted.db.dao.TrashNoteDao;
 import com.github.bazukaa.nakshatra.noted.db.entity.Note;
 import com.github.bazukaa.nakshatra.noted.db.entity.TrashNote;
 import com.github.bazukaa.nakshatra.noted.utils.NoteAsyncTask;
-
+import com.github.bazukaa.nakshatra.noted.utils.TrashNoteAsyncTask;
 import java.util.List;
 
 public class NoteRepository {
@@ -35,57 +32,15 @@ public class NoteRepository {
 
 
     public void insert(Note note) { new NoteAsyncTask(noteDao, NoteAsyncTask.INSERT_NOTE_REQUEST).execute(note); }
-
     public void update(Note note) { new NoteAsyncTask(noteDao, NoteAsyncTask.UPDATE_NOTE_REQUEST).execute(note); }
-
     public void delete(Note note) { new NoteAsyncTask(noteDao, NoteAsyncTask.DELETE_NOTE_REQUEST).execute(note); }
-
     public LiveData<List<Note>> getNotes() {
         return notes;
     }
 
-    public void insert(TrashNote trashNote) {new InsertTrashNoteTask(trashNoteDao).execute(trashNote); }
-
-    public void update(TrashNote trashNote){new UpdateTrashNoteTask(trashNoteDao).execute(trashNote);}
-
-    public void delete(TrashNote trashNote){new DeleteTrashNoteTask(trashNoteDao).execute(trashNote); }
-
+    public void insert(TrashNote trashNote) {new TrashNoteAsyncTask(trashNoteDao, TrashNoteAsyncTask.INSERT_TRASH_NOTE_REQUEST).execute(trashNote); }
+    public void update(TrashNote trashNote){new TrashNoteAsyncTask(trashNoteDao, TrashNoteAsyncTask.UPDATE_TRASH_NOTE_REQUEST).execute(trashNote);}
+    public void delete(TrashNote trashNote){new TrashNoteAsyncTask(trashNoteDao, TrashNoteAsyncTask.DELETE_TRASH_NOTE_REQUEST).execute(trashNote); }
     public LiveData<List<TrashNote>> getTrashNotes() {return trashNotes;}
 
-
-    private static class InsertTrashNoteTask extends AsyncTask<TrashNote, Void, Void>{
-        private TrashNoteDao trashNoteDao;
-
-        private InsertTrashNoteTask(TrashNoteDao trashNoteDao){this.trashNoteDao = trashNoteDao; }
-
-        @Override
-        protected Void doInBackground(TrashNote... trashNotes) {
-            trashNoteDao.insert(trashNotes[0]);
-            return null;
-        }
-    }
-
-    private static class UpdateTrashNoteTask extends AsyncTask<TrashNote, Void, Void>{
-        private TrashNoteDao trashNoteDao;
-
-        private UpdateTrashNoteTask(TrashNoteDao trashNoteDao){this.trashNoteDao = trashNoteDao; }
-
-        @Override
-        protected Void doInBackground(TrashNote... trashNotes) {
-            trashNoteDao.update(trashNotes[0]);
-            return null;
-        }
-    }
-
-    private static class DeleteTrashNoteTask extends AsyncTask<TrashNote, Void, Void>{
-        private TrashNoteDao trashNoteDao;
-
-        private DeleteTrashNoteTask(TrashNoteDao trashNoteDao){this.trashNoteDao = trashNoteDao; }
-
-        @Override
-        protected Void doInBackground(TrashNote... trashNotes) {
-            trashNoteDao.delete(trashNotes[0]);
-            return null;
-        }
-    }
 }
---
+++ app/src/main/java/com/github/bazukaa/nakshatra/noted/utils/TrashNoteAsyncTask.java
@@ -0,0 +1,32 @@
+package com.github.bazukaa.nakshatra.noted.utils;
+
+
+import android.os.AsyncTask;
+import com.github.bazukaa.nakshatra.noted.db.dao.TrashNoteDao;
+import com.github.bazukaa.nakshatra.noted.db.entity.TrashNote;
+
+public class TrashNoteAsyncTask extends AsyncTask<TrashNote,Void,Void> {
+    public static final int INSERT_TRASH_NOTE_REQUEST = 1;
+    public static final int UPDATE_TRASH_NOTE_REQUEST = 2;
+    public static final int DELETE_TRASH_NOTE_REQUEST = 3;
+
+    private TrashNoteDao trashNoteDao;
+    private int request;
+
+    public TrashNoteAsyncTask(TrashNoteDao trashNoteDao, int request) {
+        this.trashNoteDao = trashNoteDao;
+        this.request = request;
+    }
+
+    @Override
+    protected Void doInBackground(TrashNote... trashNotes) {
+        if(request == INSERT_TRASH_NOTE_REQUEST)
+            trashNoteDao.insert(trashNotes[0]);
+        else if(request == UPDATE_TRASH_NOTE_REQUEST)
+            trashNoteDao.update(trashNotes[0]);
+        else if(request == DELETE_TRASH_NOTE_REQUEST)
+            trashNoteDao.delete(trashNotes[0]);
+
+        return null;
+    }
+}
