0cc6f55dfe94ee8937a2dfab623b8ed2cb767af8
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-06-29 22:27:12 +0530

    Trash initial note coded

--- app/src/main/java/com/github/bazukaa/nakshatra/noted/db/NoteDatabase.java
+++ app/src/main/java/com/github/bazukaa/nakshatra/noted/db/NoteDatabase.java
@@ -49,12 +49,16 @@ public abstract class NoteDatabase extends RoomDatabase {
         protected Void doInBackground(Void... voids) {
             String title = "About The App";
             String note =
-                    "1. To reveal the contents of the card click on it.\n\n" +
-                            "2. To switch between Grid type and list type click the button on the top right corner of the main screen\n\n "+
+                    "1. To reveal the contents of the note click on it.\n\n" +
+                            "2. To switch between Grid type and list type click the button on the top right corner of the main screen.\n\n "+
                             "3. To create a note click on the fab at the bottom of the screen, to save a note click on the check button on the top right corner.\n\n" +
                             "4. To delete a note just swipe left or right over its card.\n\n" +
                             "5. Saved notes can also be updated by clicking on its card.\n\n" +
-                            "6. Feel free to mail me to make this app more happening by dropping down your valuable suggestion on nakshatravasugupta@gmail.com .";
+                            "6. Deleted notes can be seen in trash.\n\n" +
+                            "7. Trash is available by clicking 3 dots on top right corner of the main screen.\n\n" +
+                            "8. Trash notes can be restored by long pressing them.\n\n" +
+                            "9. Notes deleted from trash are permanently deleted.\n\n" +
+                            "10. Feel free to mail me to make this app more happening by dropping down your valuable suggestion on nakshatravasugupta@gmail.com .";
             long timeStamp = System.currentTimeMillis();
             Note initialNote = new Note(title, note, timeStamp);
             noteDao.insert(initialNote);
--- app/src/main/java/com/github/bazukaa/nakshatra/noted/db/TrashNoteDatabase.java
+++ app/src/main/java/com/github/bazukaa/nakshatra/noted/db/TrashNoteDatabase.java
@@ -1,12 +1,17 @@
 package com.github.bazukaa.nakshatra.noted.db;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.github.bazukaa.nakshatra.noted.db.dao.NoteDao;
 import com.github.bazukaa.nakshatra.noted.db.dao.TrashNoteDao;
+import com.github.bazukaa.nakshatra.noted.db.entity.Note;
 import com.github.bazukaa.nakshatra.noted.db.entity.TrashNote;
 
 @Database(entities = {TrashNote.class}, version = 1)
@@ -20,9 +25,42 @@ public abstract class TrashNoteDatabase extends RoomDatabase {
         if(trashDatabaseInstance == null){
             trashDatabaseInstance = Room.databaseBuilder(context.getApplicationContext(),
                     TrashNoteDatabase.class, "trash_note_database")
+                    .addCallback(roomDbCallBack)
                     .fallbackToDestructiveMigration()
                     .build();
         }
         return trashDatabaseInstance;
+    }
+
+    private static RoomDatabase.Callback roomDbCallBack = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateTrashDbTask(trashDatabaseInstance).execute();
+        }
+    };
+
+    private static class PopulateTrashDbTask extends AsyncTask<Void, Void, Void> {
+        private TrashNoteDao trashNoteDao;
+
+        private PopulateTrashDbTask(TrashNoteDatabase trashNoteDatabase){
+            trashNoteDao = trashNoteDatabase.trashNoteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String title = "This is Trash bin";
+            String note =
+                    "1. To reveal the contents of the note click on it.\n\n" +
+                            "2. To delete a note just swipe left or right over its card.\n\n" +
+                            "3. Trash notes can also be updated by clicking on its card.\n\n" +
+                            "4. Trash notes can be restored by long pressing them.\n\n" +
+                            "5. Notes deleted from trash are permanently deleted.\n\n" +
+                            "6. Feel free to mail me to make this app more happening by dropping down your valuable suggestion on nakshatravasugupta@gmail.com .";
+            long timeStamp = System.currentTimeMillis();
+            TrashNote initialNote = new TrashNote(title, note, timeStamp);
+            trashNoteDao.insert(initialNote);
+            return null;
+        }
     }
 }
