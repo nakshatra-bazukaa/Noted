006f3fb00dfe4dd8b0c300f57c43a352d7b68338
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-09-17 01:15:37 +0530

    Color selection enabled

--- app/src/main/java/com/github/bazukaa/nakshatra/noted/ui/makeeditnote/MakeEditNoteActivity.java
+++ app/src/main/java/com/github/bazukaa/nakshatra/noted/ui/makeeditnote/MakeEditNoteActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -29,31 +30,51 @@ public class MakeEditNoteActivity extends AppCompatActivity {
     public static final String EXTRA_TITLE = "com.github.bazukaa.nakshatra.noted.EXTRA_TITLE";
     public static final String EXTRA_NOTE = "com.github.bazukaa.nakshatra.noted.EXTRA_NOTE";
     public static final String EXTRA_TIMESTAMP = "com.github.bazukaa.nakshatra.noted.EXTRA_TIMESTAMP";
+    public static final String EXTRA_COLOR = "com.github.bazukaa.nakshatra.noted.EXTRA_COLOR";
     public static final String EXTRA_ID = "com.github.bazukaa.nakshatra.noted.EXTRA_ID";
-
-
 
     @BindView(R.id.act_makeNote_toolbar)
     Toolbar actMakeNoteToolbar;
 
     @BindView(R.id.act_makeNote_et_title)
     EditText titleEditText;
-
     @BindView(R.id.act_makeNote_et_note)
     EditText noteEditText;
-
     @BindView(R.id.act_makeNote_tv_time)
     TextView timeTextView;
 
     @BindView(R.id.act_makeNote_img_options)
     ImageView optionsMenu;
 
+    @BindView(R.id.color_img_1)
+    ImageView imageColor1;
+    @BindView(R.id.color_img_2)
+    ImageView imageColor2;
+    @BindView(R.id.color_img_3)
+    ImageView imageColor3;
+    @BindView(R.id.color_img_4)
+    ImageView imageColor4;
+    @BindView(R.id.color_img_5)
+    ImageView imageColor5;
+
+    @BindView(R.id.color_1)
+    View viewColor1;
+    @BindView(R.id.color_2)
+    View viewColor2;
+    @BindView(R.id.color_3)
+    View viewColor3;
+    @BindView(R.id.color_4)
+    View viewColor4;
+    @BindView(R.id.color_5)
+    View viewColor5;
+
+    private String selectedNoteColor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_note);
         ButterKnife.bind(this);
-
         //Setting up the toolbar
         setSupportActionBar(actMakeNoteToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -61,14 +82,11 @@ public class MakeEditNoteActivity extends AppCompatActivity {
 
         //To edit the note
         Intent intent = getIntent();
-
         if(intent.hasExtra(EXTRA_ID)){
             titleEditText.setText(intent.getStringExtra(EXTRA_TITLE));
             noteEditText.setText(intent.getStringExtra(EXTRA_NOTE));
             timeTextView.setText(intent.getStringExtra(EXTRA_TIMESTAMP));
         }
-
-//        initOptions();
     }
 
     private void saveNote(){
@@ -93,6 +111,60 @@ public class MakeEditNoteActivity extends AppCompatActivity {
 
         setResult(RESULT_OK, data);
         finish();
+    }
+    @OnClick(R.id.act_makeNote_img_options)
+    public void onOptionsClicked(){
+        LinearLayout linearLayout = findViewById(R.id.layout_options);
+        if(linearLayout.getVisibility() == View.GONE)
+            linearLayout.setVisibility(View.VISIBLE);
+        else
+            linearLayout.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.color_1)
+    public void view1Clicked(){
+        selectedNoteColor = "#FFFFFF";
+        imageColor1.setImageResource(R.drawable.ic_save);
+        imageColor2.setImageResource(0);
+        imageColor3.setImageResource(0);
+        imageColor4.setImageResource(0);
+        imageColor5.setImageResource(0);
+    }
+    @OnClick(R.id.color_2)
+    public void view2Clicked(){
+        selectedNoteColor = "#FDBE3B";
+        imageColor1.setImageResource(0);
+        imageColor2.setImageResource(R.drawable.ic_save);
+        imageColor3.setImageResource(0);
+        imageColor4.setImageResource(0);
+        imageColor5.setImageResource(0);
+    }
+    @OnClick(R.id.color_3)
+    public void view3Clicked(){
+        selectedNoteColor = "#FF4842";
+        imageColor1.setImageResource(0);
+        imageColor2.setImageResource(0);
+        imageColor3.setImageResource(R.drawable.ic_save);
+        imageColor4.setImageResource(0);
+        imageColor5.setImageResource(0);
+    }
+    @OnClick(R.id.color_4)
+    public void view4Clicked(){
+        selectedNoteColor = "#3A52FC";
+        imageColor1.setImageResource(0);
+        imageColor2.setImageResource(0);
+        imageColor3.setImageResource(0);
+        imageColor4.setImageResource(R.drawable.ic_save);
+        imageColor5.setImageResource(0);
+    }
+    @OnClick(R.id.color_5)
+    public void view5Clicked(){
+        selectedNoteColor = "#000000";
+        imageColor1.setImageResource(0);
+        imageColor2.setImageResource(0);
+        imageColor3.setImageResource(0);
+        imageColor4.setImageResource(0);
+        imageColor5.setImageResource(R.drawable.ic_save);
     }
 
     @Override
@@ -121,14 +193,6 @@ public class MakeEditNoteActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-    @OnClick(R.id.act_makeNote_img_options)
-    public void onOptionsClicked(){
-        LinearLayout linearLayout = findViewById(R.id.layout_options);
-        if(linearLayout.getVisibility() == View.GONE)
-            linearLayout.setVisibility(View.VISIBLE);
-        else
-            linearLayout.setVisibility(View.GONE);
     }
 
 }
